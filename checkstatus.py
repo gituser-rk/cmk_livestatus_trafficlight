e61e3569bfa59da747e1e346618ed3e8e0ce9b64
gituser-rk <42944375+gituser-rk@users.noreply.github.com>
2020-09-21 15:06:28 +0200

    Update checkstatus.py
    First functional version to test the queries

--- checkstatus.py
+++ checkstatus.py
@@ -1,37 +1,48 @@
 #!/usr/bin/env python3
-# Sample program for accessing Livestatus from Python
-# /opt/trafficlight/checkststus.py
+# Program for accessing Livestatus from Python
+# /opt/trafficlight/checkstatus.py
+import socket
+#import json, os, socket
 
-import json, os, socket
-
-# for local site only: file path to socket
-# address = "%s/tmp/run/live" % os.getenv("OMD_ROOT")
 # for local/remote sites: TCP address/port for Livestatus socket
-address = ("172.20.20.92", 6557)
+#HOST = '172.20.20.92'
+HOST = 'omd.ilb.int'
+PORT = 6557
 
-# connect to Livestatus
-family = socket.AF_INET if type(address) == tuple else socket.AF_UNIX
-sock = socket.socket(family, socket.SOCK_STREAM)
-
+#count of unacknowledged hosts in state CRITICAL:
+#query1 = "GET hosts\\nStats: state > 0\\nFilter: contact_groups ~ Netzwerk|Linux\\nFilter: host_acknowledged = 0\\n"
+query1 = "GET hosts\nStats: state > 0\nFilter: contact_groups ~ Netzwerk|Linux\nFilter: host_acknowledged = 0\n\n\n"
+#count of unacknowledged service errors in state CRITICAL:
+query2 = "GET services\nStats: state = 2\nFilter: contact_groups ~ Netzwerk|Linux\nFilter: service_acknowledged = 0\n\n\n"
+#count of unacknowledged service errors in state WARNING:i
+query3 = "GET services\nStats: state = 1\nFilter: contact_groups ~ Netzwerk|Linux\nFilter: service_acknowledged = 0\n\n\n"
 
 ### start query 1
-query1 = "GET \'hosts\\nOutputFormat: json\\nStats: state > 0\\nFilter: contact_groups ~ Netzwerk | Linux\\nFilter: host_acknowledged = 0\'"
-#print(query1)
-sock.connect(address)
-
+# connect to Livestatus
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 # send our request and let Livestatus know we're done
-#sock.sendall(GET 'hosts\nOutputFormat: json\nStats: state > 0\nFilter: contact_groups ~ Netzwerk \\| Linux\nFilter: host_acknowledged = 0')
-sock.sendall(query1)
-sock.shutdown(socket.SHUT_WR)
-
-# receive the reply as a JSON string
-chunks = []
-while len(chunks) == 0 or chunks[-1] != "":
-    chunks.append(sock.recv(4096))
-sock.close()
-reply = "".join(chunks)
-
-# print the parsed reply
-print("Hosts in status CRITICAL:")
-print(json.loads(reply))
+s.sendall(query1.encode('utf-8'))
+# read reply
+reply = s.recv(1024)
+# close connection
+s.close()
+print('Hosts in state CRITICAL:', reply.decode('utf-8'))
 ### end query 1
+### start query 2
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(query2.encode('utf-8'))
+reply = s.recv(1024)
+s.close()
+print('Services in state CRITICAL:', reply.decode('utf-8'))
+### end query 2
+
+### start query 3
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(query3.encode('utf-8'))
+reply = s.recv(1024)
+s.close()
+print('Services in state WARNING:', reply.decode('utf-8'))
+### end query 3
